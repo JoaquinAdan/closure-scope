29159df0059cf711b98df820b0e2204cca9a9aa4
JoaquinAdan <rodriguezjdev@gmail.com>
2022-12-07 09:10:14 -0300

    block

---
+++ src/scope/re.js
@@ -0,0 +1,26 @@
+var firstName; // undefined, declarar
+firstName = "Oscar"; // asignar
+console.log(firstName);
+
+var lastName = "David"; // declarar / asignar
+lastName = "Ana"; // reasignar
+console.log(lastName);
+
+var secondName = "David"; // declarando / asignando
+var secondName = "Ana"; // reasignando
+console.log(secondName);
+
+// Let
+let fruit = "Apple"; // declara y asignar
+fruit = "Kiwi"; // reasignar
+console.log(fruit);
+
+let fruit = "Banana";
+console.log(fruit);
+
+// Const
+
+const animal = "Dog"; // declara y asigna
+// animal = 'cat'; // reasignar no se puede
+const animal = "Snake";
+console.log(animal);
