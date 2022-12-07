c24ff39363274c2c0af1608db08e193d8a27f2e4
JoaquinAdan <rodriguezjdev@gmail.com>
2022-12-07 08:36:26 -0300

    global scope & function scope

---
+++ src/scope/global.js
@@ -0,0 +1,24 @@
+// Variables
+
+var a; // declarando
+var b = "b"; // declaramos y asignamos
+b = "bb"; // reasignacion
+var a = "aa"; // redeclaracion
+
+// Global scope
+var fruit = "Apple"; // global
+// console.log(fruit);
+
+function bestFruit() {
+//   console.log(fruit);
+}
+
+bestFruit();
+
+function countries() {
+    country = 'Colombia'; // global por error en la declaracion
+    console.log(country);
+}
+
+// console.log(country);
+countries()
